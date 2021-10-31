0aeaee52386ff290b8cae8b879b1faba9e62a76a
szabotudor <szabo.tudor@gmail.com>
2021-10-31 20:15:48 +0200

    Removed the text on the screen, that was initially added for testing purposes

--- main.cpp
+++ main.cpp
@@ -24,10 +24,6 @@ void poll_events(sf::RenderWindow &window, sf::Event &event) {
 int main() {
     sf::RenderWindow window(sf::VideoMode(256, 144), "The Snow", sf::Style::Fullscreen);
     sf::Event event;
-    sf::Text text; sf::Font font;
-    font.loadFromFile("Pixel.ttf");
-    text.setFont(font);
-    text.setString("Start writing to buffer");
 
     while (window.isOpen()) {
         poll_events(window, event);
@@ -36,7 +32,7 @@ int main() {
         window.clear(sf::Color::Transparent);
 
     //Main loop
-        window.draw(text);
+        
     //End of main loop
 
         //Displays the window
