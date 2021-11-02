9fb7ac5cb47cb0fbd99feb32a156845da81bb0a8
szabotudor <szabo.tudor@gmail.com>
2021-11-02 17:26:13 +0200

    Main engine now working

--- Include/Snow.h
+++ Include/Snow.h
@@ -11,20 +11,26 @@ using namespace std;
 
 namespace ss {
 	class Snow {
-		SDL_Window* window;
-		SDL_Surface* surface;
-		SDL_Renderer* render;
-		SDL_Event ev;
-		unsigned int frame_delay = 16;
+		SDL_Window* window = NULL;
+		SDL_Surface* surface = NULL;
+		SDL_Renderer* render = NULL;
+		SDL_Event event;
+		int frame_delay = 16;
+		Uint32 time = 0;
+		bool _run = true;
+
+		void poll_events();
 	public:
-		Snow(const char* name = "The Snow", Vector resolution = Vector(512, 288), Uint32 SDL_falgs = SDL_WINDOW_SHOWN, unsigned int framerate = 60);
+		Snow(const char* name, Vector resolution = Vector(512, 288), Uint32 SDL_falgs = SDL_WINDOW_SHOWN, unsigned int framerate = 60);
 		//Function to update the SDL2 components
 		void update();
 		//Function to clear the screen to a color
-		void clear(Uint8 r = 255, Uint8 g = 255, Uint8 b = 255, Uint8 a = 255);
+		void clear_screen(Uint8 r = 30, Uint8 g = 50, Uint8 b = 70, Uint8 a = 255);
+		//Returns the time since init
+		Uint32 get_time();
+		//Checks if the engine is still running
+		bool running();
 		//Stops all processes and background threads running in the helper library
 		void quit();
-	private:
-		bool _run;
 	};
 }
--- Source/Snow.cpp
+++ Source/Snow.cpp
@@ -1,20 +1,58 @@
 #include<Snow.h>
 
 
+void ss::Snow::poll_events() {
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
+			quit();
+		}
+	}
+}
+
 ss::Snow::Snow(const char* name, ss::Vector resolution, Uint32 SDL_flags, unsigned int framerate) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "Could not initialize SDL video: " << SDL_GetError();
+	}
+
 	window = SDL_CreateWindow(name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, resolution.x, resolution.y, SDL_flags);
 	surface = SDL_GetWindowSurface(window);
 	render = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
 
 	frame_delay = 1000 / framerate;
+	time = SDL_GetTicks();
 }
 
 void ss::Snow::update() {
+	if (!_run) {
+		return;
+	}
 	SDL_RenderPresent(render);
+	poll_events();
+
+	unsigned int delay = frame_delay - (SDL_GetTicks() - time);
+	cout << delay << endl;
+	if (delay > 0 and delay < frame_delay + 1) {
+		SDL_Delay(delay);
+	}
+	time = SDL_GetTicks();
 }
 
-void ss::Snow::clear(Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
+void ss::Snow::clear_screen(Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
 	SDL_SetRenderDrawColor(render, r, g, b, a);
 	SDL_RenderClear(render);
 	SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+}
+
+Uint32 ss::Snow::get_time() {
+	return time;
+}
+
+bool ss::Snow::running() {
+	return _run;
+}
+
+void ss::Snow::quit() {
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	_run = false;
 }
