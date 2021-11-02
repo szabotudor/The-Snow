7b229f0a36769130f6fb41666ebd0e3e7e1aa45e
szabotudor <szabo.tudor@gmail.com>
2021-11-02 16:51:46 +0200

    Created basic window render management functions

--- Include/Snow.h
+++ Include/Snow.h
@@ -11,13 +11,20 @@ using namespace std;
 
 namespace ss {
 	class Snow {
+		SDL_Window* window;
+		SDL_Surface* surface;
+		SDL_Renderer* render;
+		SDL_Event ev;
+		unsigned int frame_delay = 16;
 	public:
-		Snow();
+		Snow(const char* name = "The Snow", Vector resolution = Vector(512, 288), Uint32 SDL_falgs = SDL_WINDOW_SHOWN, unsigned int framerate = 60);
+		//Function to update the SDL2 components
+		void update();
+		//Function to clear the screen to a color
+		void clear(Uint8 r = 255, Uint8 g = 255, Uint8 b = 255, Uint8 a = 255);
 		//Stops all processes and background threads running in the helper library
 		void quit();
 	private:
-		//Functiona to update the screen graphics
-		void graphics_process();
 		bool _run;
 	};
 }
--- Source/Snow.cpp
+++ Source/Snow.cpp
@@ -1,38 +1,20 @@
 #include<Snow.h>
 
-/*
-ss::Snow::Snow() {
-	process_thread = thread(&_process, this);
-	process_thread.join();
+
+ss::Snow::Snow(const char* name, ss::Vector resolution, Uint32 SDL_flags, unsigned int framerate) {
+	window = SDL_CreateWindow(name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, resolution.x, resolution.y, SDL_flags);
+	surface = SDL_GetWindowSurface(window);
+	render = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
+
+	frame_delay = 1000 / framerate;
 }
 
-void ss::Snow::_process() {
-	cout << "process thread started" << endl;
-	while (_run);
-	process_thread.join();
+void ss::Snow::update() {
+	SDL_RenderPresent(render);
 }
 
-void ss::Snow::quit() {
-	_run = false;
-	cout << "All threads stopped" << endl;
+void ss::Snow::clear(Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
+	SDL_SetRenderDrawColor(render, r, g, b, a);
+	SDL_RenderClear(render);
+	SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
 }
-*/
-
-/*
-namespace ss {
-	bool _run = true;
-	thread process_thread(ss::_process);
-
-
-	void _process() {
-		cout << "process thread started" << endl;
-		while (_run);
-		process_thread.join();
-	}
-
-	void quit() {
-		_run = false;
-		cout << "All threads stopped" << endl;
-	}
-}
-*/
