566c0a78f2d4fe4cea7c81cf8d9f8f8345d8279d
szabotudor <szabo.tudor@gmail.com>
2021-11-02 16:32:06 +0200

    Switched to hardware accelerated draw mode

--- main.cpp
+++ main.cpp
@@ -16,26 +16,32 @@ int main(int argc, char* args[]) {
 	}
 
 	bool _run = true;
+	SDL_Event event;
 	SDL_Window* window;
 	SDL_Surface* surface;
-	SDL_Event event;
 	SDL_Renderer* renderer;
 	Uint32 current_time = SDL_GetTicks();
 
 	SDL_Surface* image = IMG_Load("preview.png");
-	SDL_Rect rect;
-	rect.w = image->w;
-	rect.h = image->h;
-	rect.x = 0;
-	rect.y = 0;
 
-	//Create window, and get it's surface
+	//Create window, and get it's surface and renderer
 	window = SDL_CreateWindow("The Snow", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920 / 4, 1080 / 4, SDL_WINDOW_SHOWN);
 	surface = SDL_GetWindowSurface(window);
+	renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
+	SDL_SetRenderDrawColor(renderer, 30, 50, 70, 255);
+
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
+	SDL_Rect rect;
+	rect.x = 0;
+	rect.y = 0;
+	rect.w = image->w;
+	rect.h = image->h;
+	SDL_FreeSurface(image);
 
 	while (_run) {
 		//Fill the window to clear previous frame
-		SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 30, 50, 70));
+		SDL_RenderClear(renderer);
+
 		//Poll window events
 		while (SDL_PollEvent(&event)) {
 			poll_event(event, _run);
@@ -45,30 +51,28 @@ int main(int argc, char* args[]) {
 
 
 		//Main loop
+
 		if (keystate[SDL_SCANCODE_UP]) {
-			rect.y += 1;
-			rect.h -= 1;
+			rect.y -= 1;
 		}
 		if (keystate[SDL_SCANCODE_DOWN]) {
-			rect.y -= 1;
-			rect.h += 1;
+			rect.y += 1;
 		}
 		if (keystate[SDL_SCANCODE_LEFT]) {
-			rect.x += 1;
-			rect.w -= 1;
+			rect.x -= 1;
 		}
 		if (keystate[SDL_SCANCODE_RIGHT]) {
-			rect.x -= 1;
-			rect.w += 1;
+			rect.x += 1;
 		}
 
-		SDL_BlitSurface(image, &rect, surface, NULL);
+		SDL_RenderCopy(renderer, texture, NULL, &rect);
+
 		//End of main loop
 
 
 
 		//Update the window to show drawn opjects
-		SDL_UpdateWindowSurface(window);
+		SDL_RenderPresent(renderer);
 
 		//Wait for delta, to have consistant FPS
 		int delay = 16 - (SDL_GetTicks() - current_time);
