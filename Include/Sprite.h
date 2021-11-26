362acf960342c71a762449ef001d5f0e4bcf7a6f
szabotudor <szabo.tudor@gmail.com>
2021-11-26 12:36:09 +0200

    Created the class and necessary members (not finished)

--- Include/Sprite.h
+++ Include/Sprite.h
@@ -1,11 +1,20 @@
 #pragma once
+#include<SDL.h>
+#include<SDL_image.h>
 #include<Vector.h>
 
 
 namespace ss {
 	class Sprite {
+		int frames, frame;
+		SDL_Window* window;
+		SDL_Renderer* render;
+		SDL_Surface* surface;
+		SDL_Texture** textures;
+		SDL_Rect rect;
 	public:
 		Vector position;
-		Sprite(char* texture);
+		Sprite(SDL_Window *window, const char* texture);
+		Sprite(SDL_Window* window, int frames, const char ** textures);
 	};
 }
--- Source/Sprite.cpp
+++ Source/Sprite.cpp
@@ -1,1 +1,21 @@
 #include<Sprite.h>
+
+ss::Sprite::Sprite(SDL_Window *window, const char* texture) {
+	frames = 0;
+	IMG_Init(IMG_INIT_PNG);
+	surface = IMG_Load(texture);
+	render = SDL_GetRenderer(window);
+	this->textures[0] = SDL_CreateTextureFromSurface(render, surface);
+	SDL_FreeSurface(surface);
+}
+
+ss::Sprite::Sprite(SDL_Window* window, int frames, const char** textures) {
+	this->frames = frames;
+	render = SDL_GetRenderer(window);
+	IMG_Init(IMG_INIT_PNG);
+	for (int i = 0; i < frames; i++) {
+		surface = IMG_Load(textures[i]);
+		this->textures[i] = SDL_CreateTextureFromSurface(render, surface);
+		SDL_FreeSurface(surface);
+	}
+}
