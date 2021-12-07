680b49b3fe438c3f660704c913a9099cf3dd8947
szabotudor <szabo.tudor@gmail.com>
2021-12-07 22:37:49 +0200

    Added different animation related functions (play, stop, pause, resume)

--- Include/Sprite.h
+++ Include/Sprite.h
@@ -10,19 +10,27 @@ namespace ss {
 	class Sprite {
 		int frames, frame, start, end;
 		float time = 0.0f, frame_delay;
-		bool playing = false;
 		SDL_Window* window;
 		SDL_Renderer* render;
 		SDL_Surface* surface;
 		SDL_Texture** textures;
 		SDL_Rect rect;
 	public:
+		bool playing = false;
 		Vector position;
 		Sprite(SDL_Window *window, const char* texture);
 		Sprite(SDL_Window* window, int frames, const char ** textures);
+		//Loads a new set of sprites, replacing the original textures
+		void load(SDL_Window* window, int frames, const char** textures);
 		//Draws the sprite on the window
 		void draw(float delta);
 		//Starts playing an animation from the start frame to the end frame at the given fps
 		void play(int start, int end, int fps);
+		//Plays the animation after a pause
+		void resume();
+		//Stops the animation and resets it
+		void stop();
+		//Pauses the animation to be resumed by play
+		void pause();
 	};
 }
