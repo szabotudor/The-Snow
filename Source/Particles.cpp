f589d335ecd8993bc9779d8b0f87d81e769954c1
szabotudor <szabo.tudor@gmail.com>
2021-12-15 15:28:41 +0200

    added missing line I forgot to add last commit

--- Source/Particles.cpp
+++ Source/Particles.cpp
@@ -11,6 +11,6 @@ void ss::ParticleEmitter::operator<<(Particle particle) {
 		throw invalid_argument("Ammount of particles given is higher than the ammount of free particle spots");
 	}
 	for (filled; filled < particle.ammount; filled++) {
-
+		particles[filled] = particle;
 	}
 }
