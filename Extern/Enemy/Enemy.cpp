d1640e6f8c3e2aba5b7802cb65530e69b57e8a40
szabotudor <szabo.tudor@gmail.com>
2022-01-05 18:19:21 +0200

    Added most of the needed functionality (appart from ai) to the enemy

--- Extern/Enemy/Enemy.cpp
+++ Extern/Enemy/Enemy.cpp
@@ -3,15 +3,54 @@
 
 Enemy::Enemy(ss::Vector position) {
 	Enemy::position = position;
-	col[0] = ss::CollisionShape(12, position);
+	collision = ss::CollisionShape(12, position);
 }
 
 void Enemy::process(double delta) {
 	delta /= 1000;
 	if (invulnerability > 0) {
 		invulnerability -= delta;
+#if defined _DEBUG
+		collision.draw_color.r = 255;
+		collision.draw_color.b = 120;
+#endif
 	}
 	else {
-
+#if defined _DEBUG
+		collision.draw_color.r = 120;
+		collision.draw_color.b = 255;
+#endif
 	}
+}
+
+void Enemy::damage() {
+	if (invulnerability <= 0) {
+		life--;
+		invulnerability = 1;
+	}
+}
+
+void Enemy::draw() {
+}
+
+bool Enemy::is_dead() {
+	return life <= 0;
+}
+
+int Enemy::get_hp() {
+	return life;
+}
+
+bool Enemy::is_invulnerable() {
+	return invulnerability >= 0;
+}
+
+void EnemyUtil::init_enemy(ss::Snow& game) {
+	init = true;
+	const char* textures[3] = {
+		"t1",
+		"t2",
+		"t3",
+	};
+	*enemy_sprite = ss::Sprite(game.get_window(), 3, textures);
 }
--- Extern/Enemy/Enemy.h
+++ Extern/Enemy/Enemy.h
@@ -2,15 +2,35 @@
 #include<Snow.h>
 
 
+static class EnemyUtil {
+public:
+	bool init = false;
+	SDL_Window* window = nullptr;
+	ss::Sprite* enemy_sprite;
+	void init_enemy(ss::Snow& game);
+};
+
+
 class Enemy {
 	ss::Vector position;
 	ss::Vector velocity;
 	ss::Vector target;
 	int life = 3;
 	double invulnerability = 1;
-	ss::CollisionShape col[3];
 public:
+	int id = 0;
+	ss::CollisionShape collision;
 	Enemy(ss::Vector position = ss::Vector(0));
 	//Process movement, damage, invulnerability, etc...
 	void process(double delta);
+	//Damages the enemy by 1 point and gives it invulnerability for a short time
+	void damage();
+	//Draw all sprites
+	void draw();
+	//Returns true if the enemy is dead
+	bool is_dead();
+	//Returns the health points (life) of the enemy
+	int get_hp();
+	//Returns true if the enemy is invulnerable
+	bool is_invulnerable();
 };
