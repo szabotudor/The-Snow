ddd3eac8a3e0dee17e8898571c75f5bb889c0c65
szabotudor <szabo.tudor@gmail.com>
2022-01-19 16:03:09 +0200

    Added a bunch of animations, effects and reactions to the enemy

--- Extern/Enemy/Enemy.cpp
+++ Extern/Enemy/Enemy.cpp
@@ -4,59 +4,110 @@
 bool init = false;
 SDL_Window* window = nullptr;
 ss::Sprite enemy_sprite;
+ss::Sprite melt_sprite;
 
 void init_enemy(ss::Snow& game) {
 	init = true;
-	const char* textures[6] = {
+	const char* textures[9] = {
 		"Sprites/Snowman/snowman_body0000.png",
 		"Sprites/Snowman/snowman_body0001.png",
 		"Sprites/Snowman/snowman_body0002.png",
+
+		"Sprites/Snowman/snowman_middle0000.png",
+		"Sprites/Snowman/snowman_middle0001.png",
+		"Sprites/Snowman/snowman_middle0002.png",
 
 		"Sprites/Snowman/snowman_head0000.png",
 		"Sprites/Snowman/snowman_head0001.png",
 		"Sprites/Snowman/snowman_head0002.png"
 	};
-	enemy_sprite = ss::Sprite(game.get_window(), 6, textures);
+	const char* melt_textures[5] = {
+		"Sprites/Snowman/melt0000.png",
+		"Sprites/Snowman/melt0001.png",
+		"Sprites/Snowman/melt0002.png",
+		"Sprites/Snowman/melt0003.png",
+		"Sprites/Snowman/melt0004.png"
+	};
+	enemy_sprite = ss::Sprite(game.get_window(), 9, textures);
+	melt_sprite = ss::Sprite(game.get_window(), 5, melt_textures);
 }
 
 
 Enemy::Enemy(ss::Vector position) {
 	Enemy::position = position;
-	collision = ss::CollisionShape(ss::Vector(17, 27), position);
+	collision = ss::CollisionShape(ss::Vector(17, 39), position);
+	rng.randomize();
+	rot_speed = rng.randf_range(0.03, 0.06);
 }
 
 void Enemy::process(double delta) {
 	delta /= 1000;
 	lifetime += delta;
-	if (lifetime > 0.8) {
-		lifetime = 0;
-		switch (look_dir) {
-		case Enemy::LookDirection::UP:
-			look_dir = LookDirection::RIGHT;
-			break;
-		case Enemy::LookDirection::DOWN:
-			look_dir = LookDirection::LEFT;
-			break;
-		case Enemy::LookDirection::LEFT:
-			look_dir = LookDirection::UP;
-			break;
-		case Enemy::LookDirection::RIGHT:
-			look_dir = LookDirection::DOWN;
-			break;
-		default:
-			break;
+	vert_velocity -= delta * 1500;
+	if (invulnerability > 0 and life < 3 and height >= 0) {
+		height += vert_velocity * delta;
+		if (height < 0) {
+			height = 0;
 		}
 	}
-	position = collision.position;
+	/*
+	anim_time += delta;
+	switch (animation) {
+	case Enemy::Anim::IDLE:
+		if (anim_time > 0.8) {
+			anim_time = 0;
+			if (draw_offset == ss::Vector(0, -1)) {
+				draw_offset = ss::Vector(0, 1);
+			}
+			else {
+				draw_offset = ss::Vector(0, -1);
+			}
+		}
+		break;
+	case Enemy::Anim::SHOOT:
+		break;
+	case Enemy::Anim::WALK:
+		break;
+	default:
+		break;
+	}
+	*/
+
+	visibility_timer += delta;
+	if (visibility_timer > 0.05) {
+		visible = !visible;
+		visibility_timer = 0;
+	}
+
+	if (invulnerability > 0 and height > 0) {
+		if (lifetime > rot_speed) {
+			lifetime = 0;
+			switch (look_dir) {
+			case Enemy::LookDirection::UP:
+				look_dir = LookDirection::RIGHT;
+				break;
+			case Enemy::LookDirection::DOWN:
+				look_dir = LookDirection::LEFT;
+				break;
+			case Enemy::LookDirection::LEFT:
+				look_dir = LookDirection::UP;
+				break;
+			case Enemy::LookDirection::RIGHT:
+				look_dir = LookDirection::DOWN;
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	position = collision.position + col_draw_offset - ss::Vector(0, height);
 	if (invulnerability > 0) {
 		invulnerability -= delta;
 #if defined _DEBUG
 		collision.draw_color.r = 255;
 		collision.draw_color.b = 120;
-#endif
 	}
 	else {
-#if defined _DEBUG
 		collision.draw_color.r = 120;
 		collision.draw_color.b = 255;
 #endif
@@ -67,6 +118,22 @@ void Enemy::damage() {
 	if (invulnerability <= 0) {
 		life--;
 		invulnerability = 1;
+		vert_velocity = 300;
+		if (life > 0) {
+			collision.position.y += 14;
+		}
+		switch (life) {
+		case 2:
+			col_draw_offset -= ss::Vector(0, 12);
+			collision.size -= ss::Vector(2, 14);
+			break;
+		case 1:
+			col_draw_offset -= ss::Vector(0, 11);
+			collision.size -= ss::Vector(2, 13);
+			break;
+		default:
+			break;
+		}
 	}
 }
 
@@ -77,46 +144,116 @@ void Enemy::draw(ss::Vector camera_offset) {
 	collision.draw();
 	collision.position += camera_offset;
 #endif
-	switch (look_dir) {
-	case Enemy::LookDirection::UP:
-		enemy_sprite.flip = SDL_FLIP_NONE;
-		enemy_sprite.frame = 2;
-		enemy_sprite.draw(1);
-		enemy_sprite.frame = 5;
-		enemy_sprite.position += ss::Vector(2, -11);
-		enemy_sprite.draw(1);
-		break;
-	case Enemy::LookDirection::DOWN:
-		enemy_sprite.flip = SDL_FLIP_NONE;
-		enemy_sprite.frame = 1;
-		enemy_sprite.draw(1);
-		enemy_sprite.frame = 4;
-		enemy_sprite.position += ss::Vector(2, -11);
-		enemy_sprite.draw(1);
-		break;
-	case Enemy::LookDirection::LEFT:
-		enemy_sprite.flip = SDL_FLIP_HORIZONTAL;
-		enemy_sprite.frame = 0;
-		enemy_sprite.draw(1);
-		enemy_sprite.frame = 3;
-		enemy_sprite.position += ss::Vector(2, -11);
-		enemy_sprite.draw(1);
-		break;
-	case Enemy::LookDirection::RIGHT:
-		enemy_sprite.flip = SDL_FLIP_NONE;
-		enemy_sprite.frame = 0;
-		enemy_sprite.draw(1);
-		enemy_sprite.frame = 3;
-		enemy_sprite.position += ss::Vector(2, -11);
-		enemy_sprite.draw(1);
-		break;
-	default:
-		break;
+	if (invulnerability > 0) {
+		melt_sprite.frame = (int)((1 - invulnerability) * 4);
+		switch (life) {
+		case 2:
+			melt_sprite.position = collision.position + ss::Vector(-2, 6) - camera_offset;
+			melt_sprite.draw(1);
+			break;
+		case 1:
+			melt_sprite.position = collision.position + ss::Vector(-3, -7) - camera_offset;
+			melt_sprite.draw(1);
+			break;
+		case 0:
+			melt_sprite.position = collision.position + ss::Vector(-4, -7) - camera_offset;
+			melt_sprite.draw(1);
+			break;
+		default:
+			break;
+		}
+	}
+	else {
+		visible = true;
+	}
+
+	if (visible) {
+		ss::Vector ofs(1, -9);
+		switch (look_dir) {
+		case Enemy::LookDirection::UP:
+			enemy_sprite.flip = SDL_FLIP_NONE;
+			if (life > 2) {
+				enemy_sprite.frame = 2;
+				enemy_sprite.draw(1);
+				enemy_sprite.position += ofs + draw_offset;
+			}
+
+			if (life > 1) {
+				enemy_sprite.frame = 5;
+				enemy_sprite.draw(1);
+				enemy_sprite.position += ofs + ss::Vector(0, 1) + draw_offset;
+			}
+
+			if (life > 0) {
+				enemy_sprite.frame = 8;
+				enemy_sprite.draw(1);
+			}
+			break;
+		case Enemy::LookDirection::DOWN:
+			enemy_sprite.flip = SDL_FLIP_NONE;
+			if (life > 2) {
+				enemy_sprite.frame = 1;
+				enemy_sprite.draw(1);
+				enemy_sprite.position += ofs + draw_offset;
+			}
+
+			if (life > 1) {
+				enemy_sprite.frame = 4;
+				enemy_sprite.draw(1);
+				enemy_sprite.position += ofs + ss::Vector(0, 1) + draw_offset;
+			}
+
+			if (life > 0) {
+				enemy_sprite.frame = 7;
+				enemy_sprite.draw(1);
+			}
+			break;
+		case Enemy::LookDirection::LEFT:
+			enemy_sprite.flip = SDL_FLIP_HORIZONTAL;
+			if (life > 2) {
+				enemy_sprite.frame = 0;
+				enemy_sprite.draw(1);
+				enemy_sprite.position += ofs + draw_offset;
+			}
+
+			if (life > 1) {
+				enemy_sprite.frame = 3;
+				enemy_sprite.draw(1);
+				enemy_sprite.position += ofs + ss::Vector(0, 1) + draw_offset;
+			}
+
+			if (life > 0) {
+				enemy_sprite.frame = 6;
+				enemy_sprite.draw(1);
+			}
+			break;
+		case Enemy::LookDirection::RIGHT:
+			enemy_sprite.flip = SDL_FLIP_NONE;
+			if (life > 2) {
+				enemy_sprite.frame = 0;
+				enemy_sprite.draw(1);
+				enemy_sprite.position += ofs + draw_offset;
+			}
+
+			if (life > 1) {
+				enemy_sprite.frame = 3;
+				enemy_sprite.draw(1);
+				enemy_sprite.position += ofs + ss::Vector(0, 1) + draw_offset;
+			}
+
+			if (life > 0) {
+				enemy_sprite.frame = 6;
+				enemy_sprite.draw(1);
+			}
+			break;
+		default:
+			break;
+		}
 	}
 }
 
 bool Enemy::is_dead() {
-	return life <= 0;
+	return life <= 0 and invulnerability <= 0;
 }
 
 int Enemy::get_hp() {
--- Extern/Enemy/Enemy.h
+++ Extern/Enemy/Enemy.h
@@ -5,9 +5,16 @@ void init_enemy(ss::Snow& game);
 
 
 class Enemy {
+	ss::RandomNumberGenerator rng;
 	ss::Vector position;
+	double height = 0;
 	ss::Vector velocity;
+	double vert_velocity = 0;
 	ss::Vector target;
+	ss::Vector col_draw_offset = ss::Vector(0, 12);
+	bool visible;
+	double visibility_timer = 0;
+	double rot_speed = 0;
 	int life = 3;
 	double invulnerability = 1;
 	double lifetime = 0;
@@ -17,6 +24,14 @@ class Enemy {
 		LEFT,
 		RIGHT
 	};
+	enum class Anim {
+		IDLE,
+		SHOOT,
+		WALK
+	};
+	Anim animation = Anim::IDLE;
+	ss::Vector draw_offset = ss::Vector(0);
+	double anim_time = 0;
 	LookDirection look_dir = LookDirection::DOWN;
 public:
 	int id = 0;
