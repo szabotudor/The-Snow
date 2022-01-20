799aca6299039da59aec852a548e6f19bdf96d92
szabotudor <szabo.tudor@gmail.com>
2022-01-20 16:51:48 +0200

    Added particles, effects and physics to the snowball

--- Extern/Snowball/Snowball.cpp
+++ Extern/Snowball/Snowball.cpp
@@ -1,5 +1,21 @@
 #include "Snowball.h"
 
+
+ss::Texture* snowball_tex;
+ss::Texture* snowball_shadow;
+SDL_Texture* snowball_p_tex;
+
+
+void init_snowballs(ss::Snow& game) {
+	snowball_tex = new ss::Texture(game.get_window(), "Sprites/Snowball/Snowball.png", SDL_BLENDMODE_BLEND);
+	snowball_shadow = new ss::Texture(game.get_window(), "Sprites/Snowball/Snowball_Shadow.png", SDL_BLENDMODE_BLEND);
+	SDL_Surface* pt_sfc = SDL_CreateRGBSurface(NULL, 2, 2, 32, NULL, NULL, NULL, NULL);
+	SDL_FillRect(pt_sfc, NULL, SDL_MapRGB(pt_sfc->format, 255, 255, 255));
+	snowball_p_tex = SDL_CreateTextureFromSurface(game.get_renderer(), pt_sfc);
+	SDL_FreeSurface(pt_sfc);
+}
+
+
 Snowball::Snowball(SDL_Window* window, ss::Vector position, ss::Vector direction, double scale) {
 	Snowball::position = position;
 	Snowball::direction = direction;
@@ -7,19 +23,79 @@ Snowball::Snowball(SDL_Window* window, ss::Vector position, ss::Vector direction
 	render = SDL_GetRenderer(window);
 	rng.randomize();
 
-	tex_big = new ss::Texture(window, "Sprites/Snowball/Snowball.png");
-	
-	SDL_Surface* pt_sfc = SDL_CreateRGBSurface(NULL, 2, 2, 32, NULL, NULL, NULL, NULL);
-	SDL_Texture* pt_tex = SDL_CreateTextureFromSurface(render, pt_sfc);
-
 	ptem = new ss::ParticleEmitter(window, position);
-	ptem->add_particle_layer(16, pt_tex, 1);
+	ptem->one_time = true;
+	ptem->add_particle_layer(8, snowball_p_tex, 0.8, 0.9);
+	ptem->particle_layer[0].velocity_damping = 2;
+	ptem->particle_layer[0].initial_direction_randomness = 1;
+	ptem->particle_layer[0].initial_velocity_min = 40;
+	ptem->particle_layer[0].initial_velocity_max = 50;
+	ptem->emission_shape = ss::ParticleEmitter::EmissionShape::CIRCLE;
+	ptem->emission_radius = 2;
+
+	collision = ss::CollisionShape(6, position, window);
 }
 
-void Snowball::update() {
+void Snowball::update(double delta, ss::Texture& ground_texture, bool**& ground_bool, long long& snow_ammount, ss::CollisionShape& player_cs) {
+	if (height < 0) {
+		visible = false;
+		ptem->position = position + ss::Vector(2);
+		ptem->update(delta * 10);
+
+		//Leave snow on the ground for each particle
+		melted = true;
+		for (int i = 0; i < ptem->get_num_of_particles(); i++) {
+			if (ptem->get_particle_lifetime(i) < ptem->particle_layer[0].lifelimit) {
+				melted = false;
+				ss::Vector p_pos = ptem->get_particle_position(i);
+				for (int x = p_pos.x - 3; x < p_pos.x + 3; x++) {
+					for (int y = p_pos.y - 3; y < p_pos.y + 3; y++) {
+						if (!ground_bool[x][y]) {
+							if (p_pos.distance_to(ss::Vector(x, y)) < 3) {
+								int r = rng.randi_range(235, 255);
+								int g = rng.randi_range(ss::clamp(235, 255, r + 10), 255);
+								ground_texture.set_pixel(ss::Vector(x, y), r, g, 255);
+								ground_bool[x][y] = true;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	else {
+		delta /= 1000;
+		height -= v_velocity * delta * 5;
+		v_velocity += delta * 24;
+		position += direction * delta * 200;
+		collision.position = position - ss::Vector(0, height);
+		if (collision.is_colliding_with(player_cs)) {
+			position.y -= height;
+			height = 0;
+			v_velocity = 0;
+		}
+	}
 }
 
-void Snowball::draw() {
-	tex_big->position = position;
-	tex_big->draw();
+void Snowball::draw(ss::Vector camera_offset) {
+	if (visible) {
+		snowball_tex->position = position - camera_offset - ss::Vector(0, height);
+		snowball_shadow->position = position - camera_offset + ss::Vector(1, 1);
+
+		snowball_shadow->draw();
+		snowball_tex->draw();
+
+		if (height < 0) {
+			ptem->position = position - camera_offset;
+			ptem->draw();
+		}
+	}
+}
+
+bool Snowball::is_melted() {
+	return melted;
+}
+
+void Snowball::free() {
+	ptem->free();
 }
--- Extern/Snowball/Snowball.h
+++ Extern/Snowball/Snowball.h
@@ -2,18 +2,30 @@
 #include<Snow.h>
 
 
+void init_snowballs(ss::Snow& game);
+
+
 class Snowball {
 	ss::RandomNumberGenerator rng;
 	ss::Vector position, direction;
+	double height = 0;
+	double v_velocity = -8;
 	SDL_Window* window;
 	SDL_Renderer* render;
 	double scale;
-	ss::Texture* tex_big;
 	ss::ParticleEmitter* ptem;
+	bool visible = true;
+	bool melted = false;
 public:
-	Snowball(SDL_Window* window, ss::Vector position, ss::Vector direction, double scale);
+	ss::CollisionShape collision;
+
+	Snowball(SDL_Window* window, ss::Vector position, ss::Vector direction, double scale = 1);
 	//Advance the snowball's position forward
-	void update();
+	void update(double delta, ss::Texture& ground_texture, bool**& ground_bool, long long& snow_ammount, ss::CollisionShape& player_cs);
 	//Draw the snowball on the window
-	void draw();
+	void draw(ss::Vector camera_offset);
+	//Returns true if the snowball hit the player or the ground
+	bool is_melted();
+	//Frees all components in the snowball
+	void free();
 };
